a8a83904b1914df624e26713fec8ac55f78e98e2
shchukins <s.v.schukin@gmail.com>
2026-04-05 22:02:03 +0300

    Initial Commit

---
+++ HumanEngineIOS/HumanEngineIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanEngineIOSApp.swift
+//  HumanEngineIOS
+//
+//  Created by Сергей Щукин on 05.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HumanEngineIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
